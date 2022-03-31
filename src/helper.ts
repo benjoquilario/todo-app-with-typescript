93b74f03bc9d725ebde95b51ad5ffed06c8e6121
iambenjo <82529126+benjoquilario@users.noreply.github.com>
2022-03-31 17:46:36 +0800

    add: helper for mapping the todo option

---
+++ src/helper.ts
@@ -0,0 +1,17 @@
+export const option = [
+   {
+      id: 1,
+      name: 'all',
+      label: 'show all todo items',
+   },
+   {
+      id: 2,
+      name: 'active',
+      label: 'show active todo items',
+   },
+   {
+      id: 3,
+      name: 'completed',
+      label: 'show completed todo items',
+   },
+];
