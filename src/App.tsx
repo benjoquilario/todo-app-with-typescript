78e41aef82d43018886bbd03b20743e73aa1df56
iambenjo <82529126+benjoquilario@users.noreply.github.com>
2022-03-31 17:48:48 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todo from './components/Todo/Todo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div className="flex justify-center bg-[#161722] dark:bg-[#f9f9f9] min-h-screen py-10 md:py-[68px] px-6 bg-[url('/public/images/bg-mobile-dark.jpg')] md:bg-[url('/public/images/bg-desktop-dark.jpg')] dark:bg-[url('/public/images/bg-mobile-light.jpg')] dark:md:bg-[url('/public/images/bg-desktop-light.jpg')] bg-[length:100%_auto] bg-no-repeat transition-all">
+         <Todo />
+      </div>
+   );
 }
 
 export default App;
