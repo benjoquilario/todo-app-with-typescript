719cec439e583cf9c4bb94fac740783d127e60ff
iambenjo <82529126+benjoquilario@users.noreply.github.com>
2022-03-31 17:47:30 +0800

    add: utilities for typescript type and interface

---
+++ src/utils.ts
@@ -0,0 +1,33 @@
+export const initialState = [
+   {
+      id: '',
+      todo: '',
+      completed: false,
+   },
+];
+
+export interface NewTodosState {
+   id: number;
+   todo: string;
+   completed: boolean;
+}
+
+export interface TodoListRender {
+   id: number;
+   todo: string;
+   completed: boolean;
+   index: number;
+   updateItem: (id: number) => void;
+   deleteItem: (id: number) => void;
+   items: NewTodosState[];
+   setItems: (data: any) => void;
+}
+
+export type NewTodoProps = {
+   onAddTodo: (value: NewTodosState) => void;
+};
+
+export const getData = () => JSON.parse(localStorage.getItem('todos') || '');
+
+export const setData = (key: string, value: any[]) =>
+   localStorage.setItem(key, JSON.stringify(value));
